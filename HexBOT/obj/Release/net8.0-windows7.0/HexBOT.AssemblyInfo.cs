f96225475e81fa75bd8e92ac1c20cf5e24483393
Umbra <69671761+Umbra999@users.noreply.github.com>
2023-12-29 06:20:59 +0100

    sex

--- HexBOT/obj/Release/net8.0-windows7.0/HexBOT.AssemblyInfo.cs
+++ HexBOT/obj/Release/net8.0-windows7.0/HexBOT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HexBOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713abfff386960f814f55bc1d0ec143641c8dfd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HexBOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HexBOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
